feb4cae1a4f41c32098074ba7fa864cac0a16389
Anastasiia <bananajw1999@gmail.com>
2025-01-31 11:02:54 +0100

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,11 @@
-function getElementWidth(content, padding, border) {
-  const con = Number.parseFloat(content);
-  const padd = Number.parseFloat(padding);
-  const bor = Number.parseFloat(border);
-  const totalwidth = con + padd * 2 + bor * 2;
-  return `${totalwidth}px`;
+function checkForSpam(message) {
+  let upperCaseMessage = message.toUpperCase();
+  return upperCaseMessage.includes(`SPAM`) || upperCaseMessage.includes(`SALE`);
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
